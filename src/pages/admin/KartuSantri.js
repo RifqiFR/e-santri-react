c7875c9655658b265aebcd497322b587c6190b8e
Mario Prasetya Mulya <marioprasetyamulya85@gmail.com>
2022-01-13 17:51:51 +0700

    add kartusantri page filter

--- src/pages/admin/KartuSantri.js
+++ src/pages/admin/KartuSantri.js
@@ -1,54 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { SANTRI as DUMMY_SANTRI, ANGKATAN } from "constants/dummies";
+import { SANTRI } from "constants/local_storage_keys";
 import { Link } from "react-router-dom";
 import { Input, Label, Table, FormGroup, Form, Button } from "reactstrap";
 
-const DUMMY_SANTRI = [
-  {
-    id: 1,
-    name: "Rifqi Fajar Ramdhani",
-    sex: "L",
-  },
-  {
-    id: 2,
-    name: "Affan Abiyyu",
-    sex: "L",
-  },
-  {
-    id: 3,
-    name: "M. Fahreza Ansori",
-    sex: "L",
-  },
-  {
-    id: 4,
-    name: "Dary Winata Nugraha",
-    sex: "L",
-  },
-  {
-    id: 5,
-    name: "Praditya Nafis M.",
-    sex: "L",
-  },
-];
+const KartuSantri = () => {
+  const [santri, setSantri] = useState([]);
+  const [displayedSantri, setDisplayedSantri] = useState([]);
+  const [filterPendidikan, setFilterPendidikan] = useState('SMA');
+  const [filterAngkatan, setFilterAngkatan] = useState(2020);
+  const [filterNama, setFilterNama] = useState('');
 
-const KartuSantri = (props) => {
+  useEffect(() => {
+    // parsing additional data from local storage
+    const additionalDataStr = localStorage.getItem(SANTRI);
+    let additionalData = [];
+
+    if (additionalDataStr) {
+      additionalData = JSON.parse(additionalDataStr);
+    }
+
+    setSantri([
+      ...DUMMY_SANTRI,
+      ...additionalData
+    ]);
+    setDisplayedSantri([
+      ...DUMMY_SANTRI,
+      ...additionalData
+    ]);
+  }, []);
+
+  const filterDataHandler = (event) => {
+    event.preventDefault();
+    setDisplayedSantri(santri.filter(s => +s.angkatan === +filterAngkatan && s.pendidikan === filterPendidikan));
+  }
+
+  const filterByName = (event) => {
+    event.preventDefault();
+
+    let word = "";
+    if (filterNama.trim().length !== 0) {
+      word = filterNama.toLowerCase();
+    }
+    
+    setDisplayedSantri(santri.filter(s => s.nama.toLowerCase().includes(word)));
+  }
+
   return (
     <>
-      <Form>
+      <Form onSubmit={filterDataHandler}>
         <FormGroup row>
           <Label sm={2} htmlFor="pendidikan" className="pr-3 mb-0">Pendidikan:</Label>
           <div sm={10}>
-            <Input name="pendidikan" id="pendidikan" type="select">
-              <option value="sma">SMA</option>
-              <option value="smp">SMP</option>
+            <Input name="pendidikan" id="pendidikan" type="select" onChange={(event) => setFilterPendidikan(event.target.value)} value={filterPendidikan}>
+              <option value="SMA">SMA</option>
+              <option value="SMP">SMP</option>
             </Input>
           </div>
         </FormGroup>
         <FormGroup row>
-          <Label sm={2} htmlFor="pendidikan" className="pr-3 mb-0">Angkatan:</Label>
+          <Label sm={2} htmlFor="angkatan" className="pr-3 mb-0">Angkatan:</Label>
           <div sm={10}>
-            <Input name="angkatan" id="angkatan" type="select">
-              <option value="2020">2020</option>
-              <option value="2021">2021</option>
+            <Input name="angkatan" id="angkatan" type="select" onChange={(event) => setFilterAngkatan(event.target.value)} value={filterAngkatan}>
+              {ANGKATAN.map(a => (
+                <option value={a}>{a}</option>
+              ))}
             </Input>
           </div>
         </FormGroup>
@@ -59,8 +75,8 @@ const KartuSantri = (props) => {
       <hr />
       <p className="text-center mb-0">Data Santri SMA Pondok Pesantren Tambak Beras Jombang</p>
       <p className="text-center">Angkatan 2020</p>
-      <Form className="d-flex justify-content-end my-2">
-        <Input className="col-3 mr-2" type="text" name="search" id="search" />
+      <Form className="d-flex justify-content-end my-2" onSubmit={filterByName}>
+        <Input className="col-3 mr-2" type="text" name="search" id="search" onChange={(event) => setFilterNama(event.target.value)} value={filterNama} />
         <Button color="secondary">Cari</Button>
       </Form>
       <Table bordered>
@@ -81,13 +97,13 @@ const KartuSantri = (props) => {
           </tr>
         </thead>
         <tbody>
-          {DUMMY_SANTRI.map((s, i) => (
+          {displayedSantri.map((s, i) => (
             <tr key={s.name}>
               <th scope="row" className="align-middle">
                 {i + 1}
               </th>
               <td className="align-middle">
-                {s.name}
+                {s.nama}
               </td>
               <td className="align-middle">
                 {s.sex}
