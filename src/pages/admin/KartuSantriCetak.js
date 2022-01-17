39f07fa4f7fbedf746b983b88561e1c6631a94d3
Rifqi Fajar Ramdani <rifqifajar24@gmail.com>
2022-01-17 17:21:09 +0700

    fix kartu santri

--- src/pages/admin/KartuSantriCetak.js
+++ src/pages/admin/KartuSantriCetak.js
@@ -4,7 +4,7 @@ import { Input, Label, Table, FormGroup, Form, Button } from "reactstrap";
 const KartuSantriCetak = (props) => {
   return (
     <>
-      <img className="w-100" src={require('../../images/kartu_santri.png')} alt="kartu_santri" />
+      <img className="w-100" src={require('../../images/Kartu Santri.png')} alt="kartu_santri" />
       <div className="d-flex align-items-center my-3">
         <p className="mb-0 mr-2">Pin: </p>
         <p className="mb-0 border py-2 px-3 rounded">746694</p>
